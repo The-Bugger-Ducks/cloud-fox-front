6f373ad8bcae58d742a2e2f22f5f715a11e30dda
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-09-14 13:39:51 -0300

    feat: creating route to dashboard
    Creating route to access the Dashboard

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
       <Route path="/home" element={<Home />} />
+
       <Route path="/login" element={<Login />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
