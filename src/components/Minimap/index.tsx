4d778980e80e4fc9823dd9821848270ccd1a7304
Giovana O. Silva <giothais.silva@gmail.com>
2022-10-04 08:40:38 -0300

    feat: add event listeners in minimap

--- src/components/Minimap/index.tsx
+++ src/components/Minimap/index.tsx
@@ -1,13 +1,42 @@
+import { LatLngLiteral } from 'leaflet';
 import 'leaflet/dist/leaflet.css'
+import { useState } from 'react';
+import mapIcon from '../../utils/Leaflet/mapIcon'
 import {
   MapContainer,
+  Marker,
+  Popup,
   TileLayer,
+  useMapEvents,
 } from 'react-leaflet'
 
 import { Container } from './styles'
 
-export default function Minimap () {
+function MinimapEvents () {
+  const [mousePos, setMousePos] = useState<LatLngLiteral>({ lat: 0, lng: 0 });
 
+  const useMap = useMapEvents({
+    click: (event) => {
+      setMousePos(event.latlng)
+    },
+  })
+
+  return (
+    <>
+        <Marker
+          position={[mousePos.lat, mousePos.lng]}
+          icon={mapIcon}
+        >
+          <Popup minWidth={90}>
+            Latitude: {mousePos.lat}, Longitude: {mousePos.lng}
+          </Popup>
+        </Marker>
+    </>
+  )
+}
+
+export default function Minimap () {
+  
   return (
     <Container>
       <MapContainer center={[-23.1895062, -45.8630127]} zoom={13} >
@@ -15,6 +44,7 @@ export default function Minimap () {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <MinimapEvents /> 
       </MapContainer>
     </Container>
   )
