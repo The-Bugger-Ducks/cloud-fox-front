4073936b7fcca2986d2f5e51a68093405d74954f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-10-09 17:38:20 -0300

    feat: create component to facebook login button

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 
 import Button from "../Button";
-import { AiFillFacebook, AiFillApple } from "react-icons/ai";
+import { AiFillApple } from "react-icons/ai";
 import { Container, Title, Buttons } from "./styles";
 
 import theme from "../../global/theme";
 
-import { AuthContext } from "../../context/AuthContext";
 import GoogleButton from "./GoogleButton";
+import FacebookButton from "./FacebookButton";
 
 export default function LoginModal() {
 	return (
@@ -15,13 +15,15 @@ export default function LoginModal() {
 			<Title>Login</Title>
 
 			<Buttons>
-				<Button
+				{/* <Button
 					title="Login com Facebook"
 					fontColor={theme.colors.white}
 					backgroundColor={"#1877F2"}
 					marginBottom={"1rem"}
 					icon={<AiFillFacebook size={24} />}
-				/>
+				/> */}
+
+				<FacebookButton />
 				<GoogleButton />
 				<Button
 					title="Login com Apple"
