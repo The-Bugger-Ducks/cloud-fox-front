a582c42fe17b981fa20e056e0fb7acbd76513ee5
Giovana T. O. Silva <giovana.silva@zukk.com.br>
2022-10-27 09:14:09 -0300

    fix: homepage responsiveness

--- src/components/CardStation/index.tsx
+++ src/components/CardStation/index.tsx
@@ -1,32 +1,25 @@
-import { useNavigate } from 'react-router-dom'
-import weather from '../../assets/weather.svg'
-import {
-  Title,
-  CardContainer,
-  Border,
-  Content,
-  Weather,
-  Description
-} from './styles'
+import { useNavigate } from "react-router-dom";
+import weather from "../../assets/weather.svg";
+import { Title, CardContainer, Border, Content, Weather, Description } from "./styles";
 
 interface CardProps {
-  id: string
-  title: string
-  description: string
+	id: string;
+	title: string;
+	description: string;
 }
 
-export default function CardChart ({ id, title, description }: CardProps) {
-  const navigate = useNavigate()
+export default function CardChart({ id, title, description }: CardProps) {
+	const navigate = useNavigate();
 
-  return (
-    <CardContainer onClick={() => navigate('/dashboard/' + id)}>
-      <Border>
-        <Content>
-          <Title>{title}</Title>
-          <Description>{description}</Description>
-        </Content>
-      </Border>
-      <Weather src={weather} alt="Tempo" />
-    </CardContainer>
-  )
+	return (
+		<CardContainer onClick={() => navigate("/dashboard/" + id)}>
+			<Border>
+				<Content>
+					<Title>{title}</Title>
+					<Description>{description}</Description>
+				</Content>
+			</Border>
+			<Weather src={weather} alt="Tempo" />
+		</CardContainer>
+	);
 }
--- src/components/CardStation/styles.ts
+++ src/components/CardStation/styles.ts
@@ -1,51 +1,56 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.black};
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  font-size: ${({ theme }) => theme.fontSizes.subtitle};
-`
+	color: ${({ theme }) => theme.colors.black};
+	font-weight: ${({ theme }) => theme.fontWeights.regular};
+	font-size: ${({ theme }) => theme.fontSizes.subtitle};
+`;
 
 export const Description = styled.p`
-  color: ${({ theme }) => theme.colors.primary_dark};
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  font-size: ${({ theme }) => theme.fontSizes.subtitle};
-  margin-left: 44%;
-`
+	color: ${({ theme }) => theme.colors.primary_dark};
+	font-weight: ${({ theme }) => theme.fontWeights.regular};
+	font-size: ${({ theme }) => theme.fontSizes.subtitle};
+	margin-left: 44%;
+`;
 
 export const CardContainer = styled.div`
-  width: 70%;
-  height: 16rem;
-  display: flex;
-  flex-direction: row-reverse;
-  position: relative;
-  margin-bottom: 3rem;
-  cursor: pointer;
-`
+	width: 15rem;
+	height: 16rem;
+	display: flex;
+	flex-direction: row-reverse;
+	position: relative;
+	margin-bottom: 3rem;
+	cursor: pointer;
+	margin: 0 2rem 2rem 0;
+`;
 
 export const Border = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 1px;
-  background: linear-gradient(145.3deg, #ee7733 6.85%, #7711bb 94.01%);
-  border-radius: 1rem;
-`
+	width: 100%;
+	height: 100%;
+	padding: 1px;
+	background: linear-gradient(145.3deg, #ee7733 6.85%, #7711bb 94.01%);
+	border-radius: 1rem;
+`;
 
 export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background: ${({ theme }) => theme.colors.white};
-  height: 100%;
-  width: 100%;
-  padding: 1.5rem;
-  border-radius: 1rem;
-`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background: ${({ theme }) => theme.colors.white};
+	height: 100%;
+	width: 100%;
+	padding: 1.5rem;
+	border-radius: 1rem;
+`;
 
 export const Weather = styled.img`
-  width: 12rem;
-  height: 11rem;
-  position: absolute;
-  top: 45%;
-  right: 50.5%;
-`
+	width: 12rem;
+	height: 11rem;
+	position: absolute;
+	top: 45%;
+	right: 50.5%;
+
+	@media screen and (max-width: 1267px) {
+		width: 8rem;
+	}
+`;
