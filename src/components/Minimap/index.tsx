7a4f3b6851625c108580bba9c7a4f4e1ff202d86
Giovana O. Silva <giothais.silva@gmail.com>
2022-10-27 16:53:31 -0300

    fix: minimap (#96)
    Co-authored-by: Giovana T. O. Silva <giovana.silva@zukk.com.br>

--- src/components/Minimap/index.tsx
+++ src/components/Minimap/index.tsx
@@ -1,66 +1,67 @@
-import { LatLngLiteral } from 'leaflet';
-import 'leaflet/dist/leaflet.css'
-import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
-import mapIcon from '../../utils/Leaflet/mapIcon'
-import {
-  MapContainer,
-  Marker,
-  TileLayer,
-  useMapEvents,
-} from 'react-leaflet'
+import { LatLngLiteral } from "leaflet";
 
-import { Container } from './styles'
-import { MinimapEventsRef } from '../../interfaces/MinimapEventsRef';
-import { MinimapRef } from '../../interfaces/Minimap';
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
+import mapIcon from "../../utils/Leaflet/mapIcon";
+import { MapContainer, Marker, TileLayer, useMapEvents, useMap } from "react-leaflet";
 
-const MinimapEvents = forwardRef<MinimapEventsRef, {}> (
-  (props, ref) => {
-    const [mousePos, setMousePos] = useState<LatLngLiteral>({ lat: 0, lng: 0 });
+import { Container } from "./styles";
+import { MinimapEventsRef } from "../../interfaces/MinimapEventsRef";
+import { MinimapRef } from "../../interfaces/Minimap";
+import "leaflet/dist/leaflet.css";
 
-    const useMap = useMapEvents({
-      click: (event) => {
-        setMousePos(event.latlng)
-      },
-    })
+const MinimapEvents = forwardRef<MinimapEventsRef, {}>((props, ref) => {
+	const [mousePos, setMousePos] = useState<LatLngLiteral>({ lat: 0, lng: 0 });
 
-    useImperativeHandle(ref, () => ({
-      getLatLng: () => { return mousePos }
-    }));
+	const useMapEvent = useMapEvents({
+		click: (event) => {
+			setMousePos(event.latlng);
+		},
+	});
 
-    return (
-      <>
-          <Marker
-            position={[mousePos.lat, mousePos.lng]}
-            icon={mapIcon}
-          />
-      </>
-    )
-  }
-) 
+	const map = useMap();
 
-const Minimap = forwardRef<MinimapRef, {}> (
-  (props, ref) => {
-    const minimapEventsRef = useRef<MinimapEventsRef>(null);
+	useEffect(() => {
+		if (map) {
+			setInterval(function () {
+				map.invalidateSize();
+			}, 100);
+		}
+	}, [map]);
 
-    useImperativeHandle(ref, () => ({
-      getLatLng: () => {
-        const latLng = minimapEventsRef.current?.getLatLng()
-        return latLng
-      }
-    }));
-    
-    return (
-      <Container>
-        <MapContainer center={[-23.1895062, -45.8630127]} zoom={13} >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <MinimapEvents ref={minimapEventsRef} /> 
-        </MapContainer>
-      </Container>
-    )
-  }
-)
+	useImperativeHandle(ref, () => ({
+		getLatLng: () => {
+			return mousePos;
+		},
+	}));
 
-export default Minimap
+	return (
+		<>
+			<Marker position={[mousePos.lat, mousePos.lng]} icon={mapIcon} />
+		</>
+	);
+});
+
+const Minimap = forwardRef<MinimapRef, {}>((props, ref) => {
+	const minimapEventsRef = useRef<MinimapEventsRef>(null);
+
+	useImperativeHandle(ref, () => ({
+		getLatLng: () => {
+			const latLng = minimapEventsRef.current?.getLatLng();
+			return latLng;
+		},
+	}));
+
+	return (
+		<Container>
+			<MapContainer center={[-23.1895062, -45.8630127]} zoom={13}>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<MinimapEvents ref={minimapEventsRef} />
+			</MapContainer>
+		</Container>
+	);
+});
+
+export default Minimap;
